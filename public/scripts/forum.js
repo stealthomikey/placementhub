003e2899019e6c0252a7ca60e020300ec9b5e36b
stealthomikey <115486033+stealthomikey@users.noreply.github.com>
2024-10-13 17:17:16 +0100

    test links work

---
+++ public/scripts/forum.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const overviewCards = document.querySelectorAll('.overviewcard');
+    const forumSections = document.querySelectorAll('.forum-section');
+  
+    // Function to hide all forum sections
+    function hideAllForums() {
+      forumSections.forEach(section => {
+        section.classList.remove('active');
+      });
+    }
+  
+    // Add click event listeners to all overview cards
+    overviewCards.forEach(card => {
+      card.addEventListener('click', function () {
+        const forumToShow = this.getAttribute('data-forum') + '-forums';
+        hideAllForums(); // Hide all forums
+        document.querySelector(`.${forumToShow}`).classList.add('active'); // Show the clicked forum
+      });
+    });
+  });
+  

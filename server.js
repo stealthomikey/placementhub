fd3935aa71654a5b99e5d114d2b8cd5c424c91dd
stealthomikey <115486033+stealthomikey@users.noreply.github.com>
2024-10-15 23:08:01 +0100

    added NHS-lanarkshire

--- server.js
+++ server.js
@@ -76,14 +76,15 @@ app.get('/socials', (req, res) => {
     res.render('pages/socials', { user: req.session.user});
 });
 
-const validLocations = ['nhs-tayside', 'nhs-shetland', 'nhs-highland', 'nhs-grampianmoray', 'nhs-grampian', 'nhs-glasgow-and-clyde'];
+const validLocations = ['nhs-tayside', 'nhs-shetland', 'nhs-highland', 'nhs-grampianmoray', 'nhs-grampian', 'nhs-glasgow-and-clyde', 'NHS-lanarkshire'];
 const capitalizedLocations = {
     'nhs-tayside': 'NHS-tayside',
     'nhs-shetland': 'NHS-shetland',
     'nhs-highland': 'NHS-highland',
     'nhs-grampianmoray': 'NHS-grampianmoray',
     'nhs-grampian': 'NHS-grampian',
-    'nhs-glasgow-and-clyde': 'NHS-glasgow-and-clyde'
+    'nhs-glasgow-and-clyde': 'NHS-glasgow-and-clyde',
+    'nhs-lanarkshire': 'NHS-lanarkshire'
 };
 
 app.get('/accommodation/:location', (req, res) => {

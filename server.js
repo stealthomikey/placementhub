ad9ca8d3806412b6063a9497e1d9191c913c8281
stealthomikey <115486033+stealthomikey@users.noreply.github.com>
2024-11-26 20:16:29 +0000

    test

--- server.js
+++ server.js
@@ -59,12 +59,22 @@ app.get('/forumpost', async (req, res) => {
     // Check if the user has voted on this post
     const userVote = userId ? post.voters.find(vote => vote.userId === userId) : null;
 
+    const isLoggedIn = req.session.loggedin || false; // Set logged in flag
+
+    // Debug logging
+    console.log('Rendering /forumpost:', {
+        user: req.session.user,
+        post: post,
+        userVote: userVote ? userVote.voteType : null,
+        isLoggedIn: isLoggedIn
+    });
+
     // Pass the data to the EJS template
     res.render('pages/forumpost', {
         user: req.session.user,
         post: post,
         userVote: userVote ? userVote.voteType : null, // Pass the vote type if logged in
-        isLoggedIn: req.session.loggedin || false // Pass a flag to indicate if the user is logged in
+        isLoggedIn: isLoggedIn // Pass a flag to indicate if the user is logged in
     });
 });
 
